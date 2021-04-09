ebc6da2bc9aac50214b20cd37016f459f8f18ab4
Gubrikk <gubrikk@yandex.ru>
2021-04-09 13:27:07 +0300

    refactor

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -13,42 +13,42 @@ function AddPlacePopup ({isOpen, onClose, onCloseOverlay, onAddPlace}) {
     });
   }
 
-	React.useEffect(() => {
-		nameRef.current.value = '';
-		linkRef.current.value = '';
-	}, [isOpen]);
-	
+  React.useEffect(() => {
+    nameRef.current.value = '';
+    linkRef.current.value = '';
+  }, [isOpen]);
+  
 
   return (
-  	<PopupWithForm
-			name="add-place"
-			title="Новое место"
-			buttonText="Добавить место"
-			isOpen={isOpen}
-    	onClose={onClose}
-			onCloseByOverlay={onCloseOverlay}
+    <PopupWithForm
+      name="add-place"
+      title="Новое место"
+      buttonText="Добавить место"
+      isOpen={isOpen}
+      onClose={onClose}
+      onCloseByOverlay={onCloseOverlay}
       onSubmit={handleSubmit}>
-		<input
-			className="popup__field popup__field_type_new-place"
-			type="text"
-			name="new-place"
-			placeholder="Название"
-			required
-	 		maxLength="30"
+    <input
+      className="popup__field popup__field_type_new-place"
+      type="text"
+      name="new-place"
+      placeholder="Название"
+      required
+       maxLength="30"
       ref={nameRef}
-		/>
-		<span className="error" id="new-place-error" />
-		<input
-			className="popup__field popup__field_type_link-image"
-			type="url"
-			name="link-image"
-			placeholder="Ссылка на картинку"
-			required
-     	ref={linkRef}
-		/>
-		<span className="error" id="link-image-error" />
-  	</PopupWithForm>
-  	);
+    />
+    <span className="error" id="new-place-error" />
+    <input
+      className="popup__field popup__field_type_link-image"
+      type="url"
+      name="link-image"
+      placeholder="Ссылка на картинку"
+      required
+       ref={linkRef}
+    />
+    <span className="error" id="link-image-error" />
+    </PopupWithForm>
+    );
 }
 
 export default AddPlacePopup;
